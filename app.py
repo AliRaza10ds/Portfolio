540b9cbab030dfe91d9caca9392821c299e72969
aliraza-bit <kaif20576@gmail.com>
2025-08-09 10:00:56 +0530

    added procfile

--- app.py
+++ app.py
@@ -20,4 +20,7 @@ def contact():
 def skills():
     return render_template('skill.html')
 
-app.run(debug=True)
+if __name__=="__main__":
+    from os import environ
+    port=int(environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
